a7ed56a65be865b4c4b16bb7dc4f4db0d4bb576b
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-03-28 12:21:57 -0500

    Fixed persistence bug on Recipe Page.

--- src/app/features/recipes/recipes.page.ts
+++ src/app/features/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../../shared/models/recipe.model';
 import { RecipeService } from '../../core/services/recipe.service';
 
@@ -12,7 +12,9 @@ export class RecipesPage implements OnInit {
 
   constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewWillEnter() {
     this.recipes = this.recipeService.getRecipes();
   }
 }
